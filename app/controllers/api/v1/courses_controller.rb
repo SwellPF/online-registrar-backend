0f44d459f36305d988e4623dd890676d4a330db2
Paul Fell <swellpf@gmail.com>
2021-06-08 19:59:23 -0400

    Changes to course controller to implement serializer

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::CoursesController < ApplicationController
 
     def index
-        @courses = Course.all
-        render json: @courses
+        courses = Course.all
+        render json: CourseSerializer.new(courses)
     end
 
     def show
-        @course = Course.find(params[:id])
-        render json: @course
+        course = Course.find(params[:id])
+        render json: CourseSerializer.new(course)
     end
 
     private
