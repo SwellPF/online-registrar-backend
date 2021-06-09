b3085263a1be41e8226ef9288d013ebd11b3c995
Paul Fell <swellpf@gmail.com>
2021-06-09 14:35:08 -0400

    Added student create controller action

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::StudentsController < ApplicationController
 
     def create
-        
+        @student = Student.new(student_params)
+        if @student.save
+            render json: @student
+        else
+            render json: {error: "Error creating student"}
+        end
     end
 
     def show
+        student = Student.find(params[:id])
+        render json: StudentSerializer.new(student)
 
     end
 
@@ -13,4 +20,9 @@ class Api::V1::StudentsController < ApplicationController
         render json: StudentSerializer.new(students)
     end
 
+
+    private
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name, :status)
 end
